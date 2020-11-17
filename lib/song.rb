eede7007d834b1d4618f7d5e23dec2e4ab45f15a
Michael Martinez <zeroeclipse@live.com>
2020-11-16 21:46:32 -0800

    commit tree fea30a61a72f8dad3a1ddd10d7a0ae669426531e

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Song
   attr_accessor :name
   attr_reader :artist
-
+  extend Memorable::ClassMethods
   @@songs = []
 
   def initialize
@@ -18,15 +18,7 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
-  def artist=(artist)
+   def artist=(artist)
     @artist = artist
   end
 
